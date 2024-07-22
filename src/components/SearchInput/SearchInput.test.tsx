2e8439c1bac23f151d8b7593604699d0d2240979
Alejandro Giraldo Duque <alejandro.giraldo@elenas.la>
2024-07-22 17:14:14 -0500

    Test: Update SearchInput test

--- src/components/SearchInput/SearchInput.test.tsx
+++ src/components/SearchInput/SearchInput.test.tsx
@@ -1,41 +1,48 @@
-
 import { fireEvent, render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
+import { useAppDispatch, useAppSelector } from '../../store';
+import { stateInitial } from '../../test-utils/storage/storage';
 import { setSearchTerm } from './../../store/actions/searchActions';
 import SearchInput from './index';
-
-
 
 const middlewares = [];
 const mockStore = configureStore(middlewares);
 
-describe('SearchInput: Suite', () => {
-  test('SearchInput: Dispatches value action when button is clicked', () => {
-   
-    const store = mockStore({});
-    store.dispatch = jest.fn();
+jest.mock('@store/index', () => ({
+  useAppDispatch: jest.fn(),
+  useAppSelector: jest.fn()
+}));
 
-    
+describe('SearchInput: Suite', () => {
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    (useAppDispatch as jest.Mock).mockReturnValue(dispatch);
+    (useAppSelector as jest.Mock).mockReturnValue({
+      term: ''
+    });
+  });
+
+  const store = mockStore(stateInitial);
+
+  test('SearchInput: Dispatches value action when button is clicked', () => {
     render(
       <Provider store={store}>
         <SearchInput />
       </Provider>
     );
 
-    
     const input = screen.getByPlaceholderText('Buscar un libro');
     const button = screen.getByText('Buscar');
-    
+
     fireEvent.change(input, { target: { value: 'Harry Potter' } });
-    
+
     fireEvent.click(button);
-    expect(store.dispatch).toHaveBeenCalledWith(setSearchTerm('Harry Potter'));
+    expect(dispatch).toHaveBeenCalledWith(setSearchTerm('Harry Potter'));
   });
 
   test('SearchInput: Updates input value when typed', () => {
-    const store = mockStore({});
-
     render(
       <Provider store={store}>
         <SearchInput />
